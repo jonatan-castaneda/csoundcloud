82d633fd3b2533266c549f99b775d9976c85ccbf
Ian Castañeda <jonatan_ian@outlook.com>
2017-05-10 10:20:23 -0500

    Versión con todos los modelos y administradores funcionando

--- csoundcloud/settings.py
+++ csoundcloud/settings.py
@@ -39,12 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'django_filters',
-    'modules.authors',
     'modules.music',
     "modules.Users",
     "modules.Artistas",
-
-
 ]
 
 MIDDLEWARE = [
--- modules/music/models.py
+++ modules/music/models.py
@@ -1,25 +1,38 @@
 from django.db import models
-from modules.authors.models import Author
+from modules.Artistas.models import Artista
 
 # Create your models here.
 GENEROS = (
-    ("RC", "Rock"),
-    ("PP", "Pop"),
-    ("CL", "Club"),
-    ("RG", "Reggaetón"),
-    ("BD", "Banda"),
-    ("EL", "Electrónica"),
-    ("OT", "Otro"),
-)
+        ("LA","Latina"),
+        ("PO","Pop"),
+        ("RO","Rock"),
+        ("IA","Indie/Alternativa"),
+        ("DA","Dance"),
+        ("HH","Hip Hop"),
+        ("RB","Rhythm and Blues"),
+        ("CO","Country"),
+        ("FA","Folk & Americana"),
+        ("ME", "Metal"),
+        ("SO", "Soul"),
+        ("JA", "Jazz"),
+        ("BL", "Blues"),
+        ("PU", "Punk"),
+        ("FU", "Funk"),
+        ("CL", "Clásica"),
+
+    )
 
 class Album(models.Model):
     id = models.AutoField(primary_key = True)
     nombre = models.CharField(max_length = 50)
     anio = models.IntegerField()
     rating = models.DecimalField(max_digits = 3, decimal_places=2, default=0.00, blank=True,null=True)
-    autor = models.ForeignKey(Author,
+    autor = models.ForeignKey(Artista,
         on_delete = models.CASCADE, null=True, blank=True)
     genero = models.CharField(choices = GENEROS, max_length = 20)
+
+    def __str__(self):
+        return "Album: %s" % (self.nombre,)
 
 
 class Cancion(models.Model):
@@ -29,3 +42,6 @@ class Cancion(models.Model):
     rating = models.DecimalField(max_digits = 3, decimal_places=2, default=0.00, blank=True,null=True)
     album = models.ForeignKey(Album, on_delete = models.CASCADE, related_name = "albums")
     genero = models.CharField(choices = GENEROS, max_length = 20)
+
+    def __str__(self):
+        return "Cancion: %s" % (self.nombre,)
