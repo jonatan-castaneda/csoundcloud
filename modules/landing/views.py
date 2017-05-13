a273f72858f2cd7d799f901f4797d7340cb01a92
Ian Castañeda <jonatan_ian@outlook.com>
2017-05-13 18:19:17 -0500

    Integración de la plantilla del dashboard

--- modules/landing/views.py
+++ modules/landing/views.py
@@ -3,3 +3,6 @@ import requests
 # Create your views here.
 def index(request):
     return render(request, "landing/index.html")
+
+def dashboard(request):
+    return render(request, "landing/dashboard.html")
