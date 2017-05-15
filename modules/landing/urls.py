712daa1a000f3faacaa0a6fce976f21fc4130ac7
Ian Castañeda <jonatan_ian@outlook.com>
2017-05-15 16:11:40 -0500

    Form para agragar artistas

--- modules/landing/urls.py
+++ modules/landing/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from .views import index, dashboard, artistas, albums, canciones
+from .views import index, dashboard, artistas, albums, canciones, add_artista, add_album
 
 urlpatterns = [
     url(r'^$', index, name="index"),
@@ -10,4 +10,6 @@ urlpatterns = [
     url(r'^artistas/(?P<id>[0-9]+)$', artistas, name="artistas_find"),
     url(r'^canciones/(?P<album>[0-9]+)$', canciones, name="canciones_find"),
     url(r'^albums/(?P<artista>[0-9]+)$',albums, name="albums_find"),
+    url(r'^artistas/add_artista/$', add_artista, name="add_artista"),
+    url(r'^albums/add_album/$', add_album, name="add_album"),
 ]
--- modules/landing/views.py
+++ modules/landing/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 import requests
 import json
+from .forms import AddArtistaForm, AddAlbumForm
 
 # Create your views here.
 def index(request):
@@ -54,4 +55,57 @@ def canciones(request):
 
     return render(request, "landing/canciones.html", {
         'canciones' : json_str,
+        })
+
+def add_artista(request):
+    form = AddArtistaForm(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            data = {
+                'nombre': form.cleaned_data['nombre'],
+                'nacionalidad' : form.cleaned_data['nacionalidad'],
+                'acerca_de': form.cleaned_data['acerca_de'],
+                'tipo_artista' : form.cleaned_data['tipo_artista'],
+                'genero' : form.cleaned_data['genero'],
+                
+            }
+            r = requests.post("http://localhost:8000/api/v1/artistas/",data=data)
+            print(r.status_code)
+            r = requests.get("http://localhost:8000/api/v1/artistas")
+            string = r.text
+            json_str = json.loads(string, encoding=None) 
+            return render(request, "landing/artistas.html",{
+                'artistas':json_str
+            })
+        
+
+    else:
+        return render(request, "landing/add_artista.html", {
+            'form':form
+        })
+
+def add_album(request):
+    form = AddAlbumForm(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            data = {
+                'nombre': form.cleaned_data['nombre'],
+                'anio' : form.cleaned_data['anio'],
+                'artista': form.cleaned_data['artista'],
+                'genero' : form.cleaned_data['genero'],
+                
+            }
+            r = requests.post("http://localhost:8000/api/v1/music/albums/",data=data)
+            print(r.status_code)
+            r = requests.get("http://localhost:8000/api/v1/music/albums")
+            string = r.text
+            json_str = json.loads(string, encoding=None) 
+            return render(request, "landing/albums.html",{
+                'albums':json_str
+            })
+        
+
+    else:
+        return render(request, "landing/add_album.html", {
+            'form':form
         })
