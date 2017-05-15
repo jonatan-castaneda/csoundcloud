fb258f99770dcc745c5422cb0da4285b7a3adedb
adriansolanorobles <adrian.solano@protonmail.com>
2017-05-15 14:09:37 -0500

    carga de archivos

--- modules/music/views.py
+++ modules/music/views.py
@@ -3,6 +3,10 @@ from rest_framework import generics, filters
 from .models import Album, Cancion
 from .serializers import AlbumSerializer,CancionSerializer
 import django_filters.rest_framework
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.parsers import FormParser,MultiPartParser
+from django.conf import settings
 
 # Create your views here.
 
@@ -30,6 +34,28 @@ class ListCanciones(generics.ListCreateAPIView):
     filter_fields = ('nombre','anio','album','genero')
     search_fields = ('nombre','genero')
 
+
+
 class DetailCanciones(generics.RetrieveUpdateDestroyAPIView):
     queryset = Cancion.objects.all()
     serializer_class = CancionSerializer
+
+class UploadFiles(APIView):
+    
+    parser_classes = (FormParser,MultiPartParser)
+    def handle_uploaded_file(self,f):
+        path = "%s/%s" % (settings.MEDIA_ROOT,str(f))
+        print(path)
+        with open(path, 'wb+') as destination:
+            for chunk in f.chunks():
+                destination.write(chunk)
+
+    def post(self,request):
+        
+        try:
+            
+            self.handle_uploaded_file(request.FILES['file'])
+        except:
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+        return Response(status=status.HTTP_200_OK)
