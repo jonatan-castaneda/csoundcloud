4412899a7b6719df744c3410429c809111276e81
Ian Castañeda <jonatan_ian@outlook.com>
2017-05-15 08:58:07 -0500

    navegación entre artistas->albums->canciones

--- modules/landing/urls.py
+++ modules/landing/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
-from .views import index, dashboard, artistas
+from .views import index, dashboard, artistas, albums, canciones
 
 urlpatterns = [
     url(r'^$', index, name="index"),
     url(r'^dashboard/$', dashboard, name="dashboard"),
     url(r'^artistas/$', artistas, name="artistas"),
+    url(r'^albums/$', albums, name="albums"),
+    url(r'^canciones/$', canciones, name="canciones"),
+    url(r'^artistas/(?P<id>[0-9]+)$', artistas, name="artistas_find"),
+    url(r'^canciones/(?P<album>[0-9]+)$', canciones, name="canciones_find"),
+    url(r'^albums/(?P<artista>[0-9]+)$',albums, name="albums_find"),
 ]
--- modules/landing/views.py
+++ modules/landing/views.py
@@ -8,7 +8,7 @@ def index(request):
 def dashboard(request):
     return render(request, "landing/dashboard.html")
 
-def artistas(request):
+def artistas(request, id=False):
     r = requests.get("http://localhost:8000/api/v1/artistas?format=json")
     string = r.text
     json_str = json.loads(string, encoding=None)
@@ -16,3 +16,27 @@ def artistas(request):
     return render(request, "landing/artistas.html", {
         'artistas' : json_str,
         })
+
+def albums(request, artista=False):
+    if artista:
+        r = requests.get("http://localhost:8000/api/v1/music/albums?format=json&autor=%s" % (artista))
+    else:
+        r = requests.get("http://localhost:8000/api/v1/music/albums?format=json")
+    string = r.text
+    json_str = json.loads(string)
+
+    return render(request, "landing/albums.html", {
+        'albums': json_str
+    })
+
+def canciones(request, album=False):
+    if album:
+        r = requests.get("http://localhost:8000/api/v1/music/canciones?format=json&album=%s" % (album))
+    else:
+        r = requests.get("http://localhost:8000/api/v1/music/canciones?format=json")
+    string = r.text
+    json_str = json.loads(string)
+
+    return render(request, "landing/canciones.html", {
+        'canciones': json_str
+    })
